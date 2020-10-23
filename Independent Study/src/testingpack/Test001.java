4e4d169d3e7b964d8c6ac762643e349152f49bbd
samhooper256 <samhooper256@gmail.com>
2020-10-23 09:41:46 -0500

    test

--- Independent Study/src/testingpack/Test001.java
+++ Independent Study/src/testingpack/Test001.java
@@ -18,6 +18,13 @@ public class Test001 {
 		
 		var e = union(Set.of(1,2,3), Set.of(1.2));
 		Number n;
+		
+		int x = 34;
+		(x) = 3;
+		
+		int b = -128;
+		System.out.println(b);
+		System.out.println((int) ((char) b));
 	}
 	
 	private static <E>  Set<E> union(Set<? extends E> s1, Set<? extends E> s2) {
